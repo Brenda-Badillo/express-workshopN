b1bc72e9ea93b14d784629d6aa1424a8d7c75472
Brenda Badillo <brendabadilloc364@gmail.com>
2022-03-04 20:23:36 -0600

    first commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ DELETE
 */ 
 app.get("/",(req,res,next) => {
     res.status(200);
-    res.send("Bienvenido");
+    res.send("Bienvenido al servidor");
 })
 app.listen(3000, ()=>{
     console.log("Server is running");
